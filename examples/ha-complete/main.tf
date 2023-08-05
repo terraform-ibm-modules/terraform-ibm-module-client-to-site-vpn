986abe1d47c57de4b8552650cec9fd2be1c423b3
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2023-08-05 06:17:25 +0100

    chore(deps): update terraform-module (#54)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- examples/ha-complete/main.tf
+++ examples/ha-complete/main.tf
@@ -40,7 +40,7 @@ resource "ibm_resource_instance" "secrets_manager" {
 # Create a secret group to place the certificate in
 module "secrets_manager_group" {
   source                   = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
-  version                  = "1.0.0"
+  version                  = "1.0.1"
   region                   = local.sm_region
   secrets_manager_guid     = local.sm_guid
   secret_group_name        = "${var.prefix}-certs"
--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -4,7 +4,7 @@
 
 module "landing_zone" {
   source           = "terraform-ibm-modules/landing-zone/ibm//patterns//vpc//module"
-  version          = "4.4.6"
+  version          = "4.5.0"
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.region
   prefix           = var.prefix
