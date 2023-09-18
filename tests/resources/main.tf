25081c6fd3355b6f4e1a5bedafe5544d02ab805e
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2023-09-18 16:37:28 +0100

    Update main.tf

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -5,7 +5,6 @@
 module "landing_zone" {
   source           = "terraform-ibm-modules/landing-zone/ibm//patterns//vpc//module"
   version          = "4.10.0"
-  ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.region
   prefix           = var.prefix
   tags             = var.resource_tags
