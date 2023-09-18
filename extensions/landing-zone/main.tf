9b179c37310748e69f1846d9d0394dbb64a9351a
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2023-09-18 17:06:11 +0100

    Update main.tf

--- extensions/landing-zone/main.tf
+++ extensions/landing-zone/main.tf
@@ -59,7 +59,7 @@ module "private_secret_engine" {
 # Create a secret group to place the certificate in
 module "secrets_manager_group" {
   source                   = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
-  version                  = "1.0.2"
+  version                  = "1.0.1"
   region                   = local.sm_region
   secrets_manager_guid     = local.sm_guid
   secret_group_name        = "${var.prefix}-certificates-secret-group"
@@ -73,7 +73,7 @@ module "secrets_manager_group" {
 module "secrets_manager_private_certificate" {
   depends_on             = [module.private_secret_engine]
   source                 = "terraform-ibm-modules/secrets-manager-private-cert/ibm"
-  version                = "1.0.1"
+  version                = "1.0.2"
   cert_name              = "${var.prefix}-cts-vpn-private-cert"
   cert_description       = "an example private cert"
   cert_template          = var.certificate_template_name
