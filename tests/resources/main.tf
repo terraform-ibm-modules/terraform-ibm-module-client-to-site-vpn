7e68a95f253cb5239fba14fd90a3b2a3dde3365e
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2023-09-18 14:23:02 +0000

    chore(deps): update terraform terraform-ibm-modules/landing-zone/ibm to v4.10.0

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -4,7 +4,7 @@
 
 module "landing_zone" {
   source           = "terraform-ibm-modules/landing-zone/ibm//patterns//vpc//module"
-  version          = "4.9.2"
+  version          = "4.10.0"
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.region
   prefix           = var.prefix
