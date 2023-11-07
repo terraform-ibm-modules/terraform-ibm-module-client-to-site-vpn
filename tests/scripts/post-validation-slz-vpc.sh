598e00c340a1f262d46b8d56867eb6f627eb251d
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2023-11-07 13:37:43 +0000

    onboard to new catalog pipeline (#132)

--- tests/scripts/post-validation-slz-vpc.sh
+++ tests/scripts/post-validation-slz-vpc.sh
@@ -1,23 +1,18 @@
 #! /bin/bash
 
 ########################################################################################################################
-## This script is used by the catalog pipeline to deploy SLZ VPC, which is a prerequisite for the client to site      ##
-## landing zone extension that is published to catalog                                                                ##
+## This script is used by the catalog pipeline to destroy the SLZ VPC, which was provisioned as a prerequisite        ##
+## for the client to site landing zone extension that is published to catalog                                         ##
 ########################################################################################################################
 
 set -e
 
-# Paths relative to base directory of script
-BASE_DIR=$(dirname "$0")
-TERRAFORM_SOURCE_DIR="../resources"
+TERRAFORM_SOURCE_DIR="tests/resources"
 TF_VARS_FILE="terraform.tfvars"
 
 (
-  # Execute script from base directory
-  cd "${BASE_DIR}"
-  echo "Destroying prerequisite SLZ VPC .."
-
   cd ${TERRAFORM_SOURCE_DIR}
+  echo "Destroying prerequisite SLZ VPC .."
   terraform destroy -input=false -auto-approve -var-file=${TF_VARS_FILE} || exit 1
 
   echo "Post-validation complete successfully"
--- tests/scripts/pre-validation-slz-vpc.sh
+++ tests/scripts/pre-validation-slz-vpc.sh
@@ -7,20 +7,17 @@
 
 set -e
 
-# Paths relative to base directory of script
-BASE_DIR=$(dirname "$0")
-TERRAFORM_SOURCE_DIR="../resources"
-JSON_FILE="../../../catalogValidationValues.json"
+DA_DIR="extensions/landing-zone"
+TERRAFORM_SOURCE_DIR="tests/resources"
+JSON_FILE="${DA_DIR}/catalogValidationValues.json"
 REGION="us-south"
 TF_VARS_FILE="terraform.tfvars"
 
 (
-  # Execute script from base directory
-  cd "${BASE_DIR}"
-  echo "Provisioning prerequisite SLZ VPC .."
-
   cd ${TERRAFORM_SOURCE_DIR}
+  echo "Provisioning prerequisite SLZ VPC .."
   terraform init || exit 1
+  # $VALIDATION_APIKEY is available in the catalog runtime
   echo "ibmcloud_api_key=\"${VALIDATION_APIKEY}\"" > ${TF_VARS_FILE}
   echo "prefix=\"c2s-slz-$(openssl rand -hex 2)\"" >> ${TF_VARS_FILE}
   echo "region=\"${REGION}\"" >> ${TF_VARS_FILE}
@@ -30,7 +27,7 @@ TF_VARS_FILE="terraform.tfvars"
   prefix_var_value=$(terraform output -state=terraform.tfstate -raw prefix)
   rg_var_name="resource_group"
   rg_var_value="${prefix_var_value}-management-rg"
-  echo "Appending '${prefix_var_name}' and '${rg_var_name}' input variable values to $(basename ${JSON_FILE}).."
+  echo "Appending '${prefix_var_name}' and '${rg_var_name}' input variable values to ${JSON_FILE}.."
   jq -r --arg prefix_var_name "${prefix_var_name}" --arg prefix_var_value "${prefix_var_value}" --arg rg_var_name "${rg_var_name}" --arg rg_var_value "${rg_var_value}" --arg region "${REGION}" '. + {($prefix_var_name): $prefix_var_value, ($rg_var_name): $rg_var_value, "prefix": $prefix_var_value, "region": $region}' "${JSON_FILE}" > tmpfile && mv tmpfile "${JSON_FILE}" || exit 1
 
   echo "Pre-validation complete successfully"
