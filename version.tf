badec4de6a6e86cba4fca3ee8a53751ad7c63068
Md Anam Raihan <Md.Anam.Raihan@ibm.com>
2024-02-08 08:10:47 +0000

    chore: update version constraints

--- version.tf
+++ version.tf
@@ -5,11 +5,11 @@ terraform {
   required_providers {
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = ">= 1.57.0, < 2.0.0"
+      version = ">= 1.59.0, < 2.0.0"
     }
     time = {
       source  = "hashicorp/time"
-      version = ">= 0.9.1"
+      version = ">= 0.9.1, < 1.0.0"
     }
   }
 }
