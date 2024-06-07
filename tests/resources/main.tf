c95bdf255abd694bf274168863ba43fd07c42489
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-06-07 15:09:27 -0400

    chore(deps): update terraform-module (#254)

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -4,7 +4,7 @@
 
 module "landing_zone" {
   source                 = "terraform-ibm-modules/landing-zone/ibm//patterns//vpc//module"
-  version                = "5.24.0"
+  version                = "5.24.1"
   region                 = var.region
   prefix                 = var.prefix
   tags                   = var.resource_tags
