bae9ca2360281e3c076776ec40ea606db26560a9
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-10-20 01:52:43 +0100

    fix(deps): update terraform ibm to latest for the landing zone da extension (#324)

--- extensions/landing-zone/version.tf
+++ extensions/landing-zone/version.tf
@@ -4,7 +4,7 @@ terraform {
     # Locking into an exact version for a deployable architecture
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.70.0"
+      version = "1.70.1"
     }
     time = {
       source  = "hashicorp/time"
