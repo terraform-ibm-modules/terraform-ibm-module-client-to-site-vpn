64f5c826e731795871520d0176cf892d6eaac773
akocbek@ie.ibm.com <akocbek@ie.ibm.com>
2024-12-03 19:56:37 +0000

    addressed PR comments

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -88,7 +88,7 @@ func TestRunHAUpgrade(t *testing.T) {
 	}
 }
 
-func TestQuickstartSolutionInSchematics(t *testing.T) {
+func TestQuickstartSolution(t *testing.T) {
 	t.Parallel()
 	// ------------------------------------------------------------------------------------------------------
 	// Create SLZ VPC, SM private certificate, resource group first
@@ -154,82 +154,6 @@ func TestQuickstartSolutionInSchematics(t *testing.T) {
 	// // Check if "DO_NOT_DESTROY_ON_FAILURE" is set
 	envVal, _ := os.LookupEnv("DO_NOT_DESTROY_ON_FAILURE")
 	// // Destroy the temporary existing resources if required
-	if t.Failed() && strings.ToLower(envVal) == "true" {
-		fmt.Println("Terratest failed. Debug the test and delete resources manually.")
-	} else {
-		logger.Log(t, "START: Destroy (existing resources)")
-		terraform.Destroy(t, existingTerraformOptions)
-		terraform.WorkspaceDelete(t, existingTerraformOptions, prefix)
-		logger.Log(t, "END: Destroy (existing resources)")
-	}
-}
-
-func TestQuickstartSolutionInSchematics2(t *testing.T) {
-	t.Parallel()
-	// ------------------------------------------------------------------------------------------------------
-	// Create SLZ VPC, resource group first
-	// ------------------------------------------------------------------------------------------------------
-
-	prefix := fmt.Sprintf("cts-qs-%s", strings.ToLower(random.UniqueId()))
-	realTerraformDir := "./resources"
-	tempTerraformDir, _ := files.CopyTerraformFolderToTemp(realTerraformDir, fmt.Sprintf(prefix+"-%s", strings.ToLower(random.UniqueId())))
-
-	// Verify ibmcloud_api_key variable is set
-	checkVariable := "TF_VAR_ibmcloud_api_key"
-	val, present := os.LookupEnv(checkVariable)
-	require.True(t, present, checkVariable+" environment variable not set")
-	require.NotEqual(t, "", val, checkVariable+" environment variable is empty")
-
-	// Programmatically determine region to use based on availability
-	region, _ := testhelper.GetBestVpcRegion(val, "../common-dev-assets/common-go-assets/cloudinfo-region-vpc-gen2-prefs.yaml", "eu-de")
-
-	logger.Log(t, "Tempdir: ", tempTerraformDir)
-	existingTerraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: tempTerraformDir,
-		Vars: map[string]interface{}{
-			"prefix":        prefix,
-			"region":        region,
-			"resource_tags": []string{"test-schematic"},
-		},
-		// Set Upgrade to true to ensure latest version of providers and modules are used by terratest.
-		// This is the same as setting the -upgrade=true flag with terraform.
-		Upgrade: true,
-	})
-
-	terraform.WorkspaceSelectOrNew(t, existingTerraformOptions, prefix)
-	_, existErr := terraform.InitAndApplyE(t, existingTerraformOptions)
-
-	if existErr != nil {
-		assert.True(t, existErr == nil, "Init and Apply of temp resources (SLZ VPC and Secrets Manager) failed")
-	} else {
-		// ------------------------------------------------------------------------------------
-		// Deploy VPN solution
-		// ------------------------------------------------------------------------------------
-		options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
-			Testing:      t,
-			TerraformDir: standardFlavorDir,
-			// Do not hard fail the test if the implicit destroy steps fail to allow a full destroy of resource to occur
-			ImplicitRequired: false,
-			TerraformVars: map[string]interface{}{
-				// "ibmcloud_api_key":                      options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"],
-				// "prefix":                                options.Prefix,
-				"use_existing_resource_group":           true,
-				"resource_group_name":                   terraform.Output(t, existingTerraformOptions, "resource_group_name"),
-				"existing_vpc_crn":                      terraform.Output(t, existingTerraformOptions, "management_vpc_crn"),
-				"existing_secrets_manager_instance_crn": permanentResources["secretsManagerCRN"],
-				"provider_visibility":                   "public",
-				// "cert_common_name":                      fmt.Sprintf("%s%s", options.Prefix, ".com"),
-				"certificate_template_name": permanentResources["privateCertTemplateName"],
-			},
-		})
-		output, err := options.RunTestConsistency()
-		assert.Nil(t, err, "This should not have errored")
-		assert.NotNil(t, output, "Expected some output")
-	}
-
-	// Check if "DO_NOT_DESTROY_ON_FAILURE" is set
-	envVal, _ := os.LookupEnv("DO_NOT_DESTROY_ON_FAILURE")
-	// Destroy the temporary existing resources if required
 	if t.Failed() && strings.ToLower(envVal) == "true" {
 		fmt.Println("Terratest failed. Debug the test and delete resources manually.")
 	} else {
