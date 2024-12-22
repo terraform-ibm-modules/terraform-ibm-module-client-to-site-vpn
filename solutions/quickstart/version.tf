72e8d5794d660f4c3c97947ba58a6b50e163e8fe
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-12-22 08:43:03 +0000

    fix(deps): update terraform ibm to latest for deployable architectures

--- solutions/quickstart/version.tf
+++ solutions/quickstart/version.tf
@@ -4,7 +4,7 @@ terraform {
     # Locking into an exact version for a deployable architecture
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.72.2"
+      version = "1.73.0"
     }
     time = {
       source  = "hashicorp/time"
